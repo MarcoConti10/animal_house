c919fe29de0944993ba40e32308e60fdcc4be538
Pierpaolo <pierpaolo.faustini@gmail,com>
2022-12-20 16:57:17 +0100

    Created initial landing page

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+// Initial settings
 const express = require('express')
 const app = express()
 
+// In the <index.html> file, use the image <animalHouseLogo> under the /public/images path
+// Use all the files found in public/html and public/css
+app.use(express.static(__dirname + '/public/html'));
+app.use(express.static(__dirname + '/public/css'));
+app.use(express.static(__dirname + '/public/img'));
+
+// Show <index.html> when going to the / path 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.sendFile(__dirname + '/index.html')
 })
 
+// Go to localhost:3000 to visualize the website
 app.listen(3000, () => {
-    console.log("App listening on port 3000 started")
+    console.log('App listening on port 3000')
 })
