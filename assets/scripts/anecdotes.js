e950e3006920b5bc206a4e20d2c0bf3540ffd914
Pierpaolo <pierpaolo.faustini@gmail,com>
2023-01-17 19:24:30 +0100

    testing merge conflicts

--- assets/scripts/anecdotes.js
+++ assets/scripts/anecdotes.js
@@ -9,6 +9,7 @@ getAnecdotes = async () => {
     // modify DOM here
     if (response.status == 200) {
 
+        // try without const too
         data = await response.json()
 
         // Clear before showing all anecdotes to avoid showing them twice
