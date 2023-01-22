eaea44be611960677b70e0c56a793d00810b4ced
MarcoConti10 <MarcoConti10>
2023-01-22 14:50:15 +0100

    fixed video, addeed header and footer

--- assets/scripts/randvideo.js
+++ assets/scripts/randvideo.js
@@ -1,18 +1,20 @@
-const API_KEY = "AIzaSyCo-NoyfX2PMR-RM8pQW_7-wO-cRunvqao";
-const searchTerm = 'funny animals';
+async function getRandomVideo() {
+  const API_KEY = "AIzaSyCo-NoyfX2PMR-RM8pQW_7-wO-cRunvqao";
+  const searchTerm = 'funny animals';
 
-// Recupera i video dall'API di YouTube
-fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&key=${API_KEY}`)
-  .then(response => response.json())
-  .then(data => {
-    // Seleziona un video casuale dall'elenco di video restituiti
+  // Get the video fram Youtube API
+  const response = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&key=${API_KEY}`);
+  const data = await response.json();
+
+  // Select random video from the API
   const items = data.items;
   const randomVideoIndex = Math.floor(Math.random() * items.length);
   const videoId = items[randomVideoIndex].id.videoId;
-    
-    // Inserisci il video nella pagina HTML
-    const videoContainer = document.getElementById('video-container');
-    videoContainer.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" 
-    allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-  })
-  .catch(error => console.log(error));
+  
+  // Insert video in the HTML page
+  const videoContainer = document.getElementById('video-container');
+  videoContainer.innerHTML = `<iframe src="https://www.youtube.com/embed/${videoId}" frameborder="0" 
+  allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+}
+
+getRandomVideo();
